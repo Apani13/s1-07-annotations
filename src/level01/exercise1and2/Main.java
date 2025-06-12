d221d1164e7b3de6191b820ad788569a8039288b
Andrea <andreapan98@gmail.com>
2025-06-12 13:53:38 +0200

    refactor: change relation between AppController and Main after AppService is created

--- src/level01/exercise1and2/Main.java
+++ src/level01/exercise1and2/Main.java
@@ -1,4 +1,13 @@
 package level01.exercise1and2;
 
+import level01.exercise1and2.app.AppController;
+import level01.exercise1and2.app.AppService;
+
 public class Main {
+    public static void main(String[] args) {
+
+        AppController appController = new AppController();
+
+        System.out.println(appController.run());
+    }
 }
--- src/level01/exercise1and2/app/AppController.java
+++ src/level01/exercise1and2/app/AppController.java
@@ -1,4 +1,24 @@
 package level01.exercise1and2.app;
 
+import level01.exercise1and2.models.Employee;
+
+import java.util.List;
+
 public class AppController {
+
+    public StringBuilder run() {
+
+        AppService appService = new AppService();
+        StringBuilder message = new StringBuilder();
+
+        List<Employee> employees = appService.createEmployees();
+
+        message.append("LISTING EMPLOYEES...\n");
+        message.append(appService.printEmployees(employees));
+
+        message.append("GETTING DEPRECATED METHODS...\n");
+        message.append(appService.callDeprecatedMethods(employees));
+
+        return message;
+    }
 }
