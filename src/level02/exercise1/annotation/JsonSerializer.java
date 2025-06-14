27dd8ad8736fd16245b659e044fc7f14ad205177
Andrea <andreapan98@gmail.com>
2025-06-14 16:48:14 +0200

    feat: implementation of JsonSerializer Class of exercise 1 level 2

---
+++ src/level02/exercise1/annotation/JsonSerializer.java
@@ -0,0 +1,12 @@
+package level02.exercise1.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public@interface  JsonSerializer {
+    String directory();
+}
